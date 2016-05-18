dce8bb22a2db9f6efd06f173f7301bfdb5ffc0c5
Ku Lok Sun <sundayku199911@yahoo.com.hk>
2016-05-18 22:50:57 +0800

    Return object instead of print the string directly

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ let config = require('./config.json');
 
 let user = config.user;
 let token = config.token;
-
-const owner = 'stepcase';
-const repo = 'lifehack-core';
+let repos = config.repos;
 
 function requestAsync(settings){
   return new Promise((resolve, reject) => {
@@ -27,54 +25,57 @@ function requestAsync(settings){
   });
 }
 
-let getPullRequest = {
-  'method' : 'GET',
-  'url' : 'https://api.github.com/repos/' + owner + '/' + repo + '/pulls',
-  'headers' : {
-    'User-Agent' : 'Bitbar-Pull-Request'
-  },
-  'auth' : {
-    'user' : config.user,
-    'pass' : config.token,
-    'sendImmediately': true
-  }
-};
+let promises = _
+  .map(repos, (repo) => {
+    return requestAsync({
+      'method' : 'GET',
+      'url' : 'https://api.github.com/repos/' + repo + '/pulls',
+      'headers' : {
+        'User-Agent' : 'Bitbar-Github-Pull-Request-Plugin'
+      },
+      'auth' : {
+        'user' : config.user,
+        'pass' : config.token,
+        'sendImmediately': true
+      }
+    })
+  });
 
-requestAsync(getPullRequest)
-  .then((data) => {
-    let groups = _
-      .chain(data)
-      .map( (pullRequest) => _.pick(pullRequest, 'milestone', 'assignee', 'user', 'html_url', 'title') )
-      .map( (pullRequest) => {
-        if (pullRequest.milestone) {
-          pullRequest.milestone = pullRequest.milestone.title;
-        }
-        if (pullRequest.assignee) {
-          pullRequest.assignee = pullRequest.assignee.login;
-        }
-        if (pullRequest.user) {
-          pullRequest.user = pullRequest.user.login;
-        }
-        return pullRequest;
-      })
-      .filter((pullRequest) => pullRequest.assignee == user || pullRequest.user == user)
-      .groupBy('milestone')
-      .value();
-    let sortedGroups = _
-      .chain(groups)
-      .keys()
-      .sort()
-      .map((milestone) => _.pick(groups, milestone))
-      .value();
-    // to strings
-    var string = _
-      .chain(sortedGroups)
-      .map((group)=>{
-        let pair = [
-          _.keys(group),
-          _
-            .chain(group)
-            .map((pullRequests)=>{
+
+Promise
+  .all(promises)
+  .then(function(responses) { 
+    let model = _.map(responses, (response) => {
+      let groups = _
+        .chain(response)
+        .map( (pullRequest) => _.pick(pullRequest, 'milestone', 'assignee', 'user', 'html_url', 'title') )
+        .map( (pullRequest) => {
+          if (pullRequest.milestone) {
+            pullRequest.milestone = pullRequest.milestone.title;
+          }
+          if (pullRequest.assignee) {
+            pullRequest.assignee = pullRequest.assignee.login;
+          }
+          if (pullRequest.user) {
+            pullRequest.user = pullRequest.user.login;
+          }
+          return pullRequest;
+        })
+        .filter((pullRequest) => pullRequest.assignee == user || pullRequest.user == user)
+        .groupBy('milestone')
+        .value();
+      let sortedGroups = _
+        .chain(groups)
+        .keys()
+        .sort()
+        .map((milestone) => _.pick(groups, milestone))
+        .value();
+      return _
+        .chain(sortedGroups)
+        .map((group)=>{
+          return {
+            'milestone' : _.keys(group),
+            'pullRequests' : _.map(group, (pullRequests)=>{
               return _
                 .chain(pullRequests)
                 .map((pullRequest)=>{
@@ -82,14 +83,10 @@ requestAsync(getPullRequest)
                 })
                 .value();
             })
-            .join('\n')
-            .value()
-        ];
-        return pair;
-      })
-      .flatten()
-      .join('\n')
-      .value();
-    console.log(string);
+          };
+        })
+        .value();
+    })
+    console.log(model);
   })
-  .catch((error) => console.log(error));
+  .catch((error) => {throw error});
